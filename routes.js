653764713744fb3ef167560ca61a9628505a8b85
cj2024 <126644942+cj2024@users.noreply.github.com>
2023-06-09 11:21:23 +0530

    code clean up

---
+++ routes.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    const messages = fs.readFileSync("message.txt", "utf8").trim().split("\n");
+    const displayMessage = messages[messages.length - 1];
+
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(`<p>${displayMessage}</p>`);
+    res.write(
+      "<body><form action=/message method=POST><input type=text name=message><button type=submit>Send</button></form></body>"
+    );
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message + "\n");
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+};
+
+module.exports = requestHandler;
+
+// module.exports.handler = requestHandler;
+
+// exports.handler = requestHandler;
