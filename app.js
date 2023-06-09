36f840e785e5195d7886045e5b6390ffaaa166cf
cj2024 <126644942+cj2024@users.noreply.github.com>
2023-06-09 11:21:01 +0530

    Code clean up task

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  console.log("Chinmay Joshi");
-  res.end("Chinmay Joshi"); // Used to send a response back to the browser
-});
+const routes = require("./routes");
+
+const server = http.createServer(routes.handler);
 
 server.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
+
