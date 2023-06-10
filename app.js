bf41d91c52462622756d6af5f674a1630a2f8c18
cj2024 <126644942+cj2024@users.noreply.github.com>
2023-06-10 11:30:44 +0530

    Routes and filters used

--- app.js
+++ app.js
@@ -4,25 +4,20 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action=/product method=POST><input type=text name=title><input type=text name=size><button type=submit>Add Product</button></form>"
-  );
-});
+app.use("/admin", adminRoute);
+app.use("/shop", shopRoute);
 
-app.post("/product", (req, res) => {
-  const { title, size } = req.body;
-  console.log("Title :", title, ",", "Size :", size);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1> Hello from Express</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
+
 
