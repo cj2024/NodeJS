53c9f1286510f10c53b6e07ee517e9de40c71bd8
cj2024 <126644942+cj2024@users.noreply.github.com>
2023-06-10 10:55:54 +0530

    Used body-parser for the first time

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const http = require("http");
+const express = require("express");
+const port = 3000;
+const bodyParser = require("body-parser");
 
-const server = http.createServer((req, res) => {
-  console.log("Chinmay Joshi");
-  res.end("Chinmay Joshi"); // Used to send a response back to the browser
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action=/product method=POST><input type=text name=title><input type=text name=size><button type=submit>Add Product</button></form>"
+  );
 });
 
-server.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.post("/product", (req, res) => {
+  const { title, size } = req.body;
+  console.log("Title :", title, ",", "Size :", size);
+  res.redirect("/");
 });
+
+app.use("/", (req, res, next) => {
+  res.send("<h1> Hello from Express</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
+
