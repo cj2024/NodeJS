57d9400344bd9727bb0180e507e3765c9df2de45
cj2024 <126644942+cj2024@users.noreply.github.com>
2023-08-21 20:49:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const http = require("http");
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-  console.log("Chinmay Joshi");
-  res.end("Chinmay Joshi"); // Used to send a response back to the browser
-});
+const express = require("express");
+const bodyParser = require("body-parser");
 
-server.listen(4000, () => {
-  console.log("Server is running on port 4000");
-});
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+
+const app = express();
+
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then((result) => {
+    app.listen(3000, () => {
+      console.log("App is running on port 3000");
+    });
+  })
+  .catch((err) => console.log(err));
+
+
